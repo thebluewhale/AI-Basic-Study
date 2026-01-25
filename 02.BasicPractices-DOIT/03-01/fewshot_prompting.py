5df82fa2c872a80952a67a04842e1a2107a6b482
Jeehwan Kim <bluewhale.marc@gmail.com>
2026-01-25 23:51:34 +0900

    one/fewshot prompting example is evaluated

--- 02.BasicPractices-DOIT/03-01/fewshot_prompting.py
+++ 02.BasicPractices-DOIT/03-01/fewshot_prompting.py
@@ -10,16 +10,25 @@ client = OpenAI(api_key=api_key)
 response = client.chat.completions.create(
   model="gpt-4o-mini",
   temperature=0.9,  # ③
-  messages=[
-    {"role": "system", "content": "너는 유치원 학생이야. 유치원생처럼 답변해줘."},
-    {"role": "user", "content": "참새"},
-    {"role": "assistant", "content": "짹짹"},
-    {"role": "user", "content": "말"},
-    {"role": "assistant", "content": "히이잉"},
-    {"role": "user", "content": "개구리"},
-    {"role": "assistant", "content": "개굴개굴"},
-    {"role": "user", "content": "뱀"},
-  ]		# ④
+  # messages=[
+  #   {"role": "system", "content": "너는 유치원 학생이야. 유치원생처럼 답변해줘."},
+  #   {"role": "user", "content": "참새"},
+  #   {"role": "assistant", "content": "짹짹"},
+  #   {"role": "user", "content": "말"},
+  #   {"role": "assistant", "content": "히이잉"},
+  #   {"role": "user", "content": "개구리"},
+  #   {"role": "assistant", "content": "개굴개굴"},
+  #   {"role": "user", "content": "뱀"},
+  # ]		# ④
+  messages = [
+      {"role": "system", "content": "you are helpful assistant"},
+      {"role": "user", "content" :
+                          "- 그 사람에게 반했어. 그 사람은 정말 멋지고 매력적이야: [분석] 긍정적, 열정," \
+                          "- 사랑이란 건 정말 복잡해. 때로는 행복하고 때로는 아프다: [분석] 복합적, 갈등" \
+                          "- 우리가 헤어진 후, 나는 사랑이란 더 이상 믿을 것이 못 된다고 느껴: [분석] 부정적, 실망감" \
+                          "앞선 예시와 마찬가지로, 다음 문장을 분석해 주세요." \
+                          "- 사랑은 때로는 어려움을 안겨주지만, 그 어려움을 함께 극복하는 게 가장 중요한 거야:"}
+  ]
 )
 
 print(response)
--- 02.BasicPractices-DOIT/03-01/oneshot_prompting.py
+++ 02.BasicPractices-DOIT/03-01/oneshot_prompting.py
@@ -8,14 +8,18 @@ client = OpenAI(api_key=api_key)
 
 # ② 
 response = client.chat.completions.create(
-  model="gpt-4o",
+  model="gpt-4o-mini",
   temperature=0.9,  # ③
-  messages=[
-    {"role": "system", "content": "너는 유치원 학생이야. 유치원생처럼 답변해줘."},
-    {"role": "user", "content": "참새"},
-    {"role": "assistant", "content": "짹짹"},
-    {"role": "user", "content": "오리"},
-  ]		# ④
+  # messages=[
+  #   {"role": "system", "content": "너는 유치원 학생이야. 유치원생처럼 답변해줘."},
+  #   {"role": "user", "content": "참새"},
+  #   {"role": "assistant", "content": "짹짹"},
+  #   {"role": "user", "content": "오리"},
+  # ]		# ④
+  messages = [
+      {"role": "system", "content": "you are helpful asssitant"},
+      {"role": "user", "content": "다음 글의 스타일을 반영해, “강아지”에 대한 글을 작성해 주세요. - 예시: 고양이는 독립적인 성향이 강합니다. 그들은 홀로 시간을 보내는 것을 즐기며, 때로는 주인의 관심이나 도움 없이도 잘 지낼 수 있습니다."},
+  ]
 )
 
 print(response)
